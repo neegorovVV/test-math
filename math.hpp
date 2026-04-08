a3853b53ed81c4b57de32b31881a686a10dd2f48
neegorovVV <kolakolaegorov@yandex.ru>
2026-04-08 15:59:52 +0300

    Update math.hpp

--- math.hpp
+++ math.hpp
@@ -1,12 +1,55 @@
 #ifndef LIBMATH_MATH_HPP
 #define LIBMATH_MATH_HPP
 
-#include <climits>   // для LLONG_MAX
+#include <climits>
 
 namespace math {
 
-// Возведение в степень с проверкой переполнения
-long long exponentiation(int base, int exp, bool& error) {
+
+long long add(int a, int b, bool& error) {
+    if ((a > 0 && b > 0 && a > LLONG_MAX - b) ||
+        (a < 0 && b < 0 && a < LLONG_MIN - b)) {
+        error = true;
+        return 0;
+    }
+    return a + b;
+}
+
+
+long long subtract(int a, int b, bool& error) {
+    if ((b > 0 && a < LLONG_MIN + b) ||
+        (b < 0 && a > LLONG_MAX + b)) {
+        error = true;
+        return 0;
+    }
+    return a - b;
+}
+
+
+long long multiply(int a, int b, bool& error) {
+    if (a != 0 && b != 0) {
+        if ((a > 0 && b > 0 && a > LLONG_MAX / b) ||
+            (a < 0 && b < 0 && a < LLONG_MAX / b) ||
+            (a > 0 && b < 0 && b < LLONG_MIN / a) ||
+            (a < 0 && b > 0 && a < LLONG_MIN / b)) {
+            error = true;
+            return 0;
+        }
+    }
+    return a * b;
+}
+
+
+long long divide(int a, int b, bool& error) {
+    if (b == 0) {
+        error = true;
+        return 0;
+    }
+    return a / b;
+}
+
+
+long long power(int base, int exp, bool& error) {
     long long result = 1;
     for (int i = 0; i < exp; i++) {
         if (result > LLONG_MAX / base) {
@@ -18,7 +61,7 @@ long long exponentiation(int base, int exp, bool& error) {
     return result;
 }
 
-// Факториал с проверкой переполнения
+
 long long factorial(int n, bool& error) {
     if (n < 0) {
         error = true;
@@ -37,6 +80,6 @@ long long factorial(int n, bool& error) {
     return result;
 }
 
-} // namespace math
+}
 
-#endif // LIBMATH_MATH_HPP
+#endif
